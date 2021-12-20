6d38c912104b9d22416275acea9a94259a0cc7e4
yasmin203 <anayasoo2031995@gmail.com>
2021-12-20 16:33:42 +0300

    init1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Creative = styled.div`
   height: 500px;
-  background: url("/images/creative/about-bg.jpg");
+  background: url("images/creative/about-bg.jpg");
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
